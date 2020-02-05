44662eed0df45eeccf3e00f42817d25048921bb3
crystal.lin <crystal.lin@acer.com>
2020-02-05 14:48:51 +0800

    Add output

--- tools/venv.sh
+++ tools/venv.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 
-CURRENT=$PWD
-ENVPATH=${CURRENT}/venv
-INSTALL_FILE=${CURRENT}/requirements.txt
+CURRENT="$PWD"
+ENVPATH="${CURRENT}/venv"
+INSTALL_FILE="${CURRENT}/requirements.txt"
+OUTPUT="$PWD/nnioutput"
+CONFIG="${ENVPATH}/bin/activate"
+
 virtualenv -p python3 ${ENVPATH}
 
+echo "export NNI_OUTPUT_DIR=${OUTPUT}" | tee -a ${CONFIG}
 echo "Start nni env"
 source ${ENVPATH}/bin/activate
 
@@ -16,3 +20,4 @@ pip3 install -r ${INSTALL_FILE}
 cd ${CURRENT}
 
 deactivate
+
